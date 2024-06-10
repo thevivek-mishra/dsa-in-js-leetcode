0f236d64a5c6791044d1acce0d2cd723bde09580
Vivek Kumar Mishra <155726934+thevivek-mishra@users.noreply.github.com>
2024-06-10 22:06:02 +0530

    Time: 86 ms (39.65%), Space: 62.1 MB (82.40%) - LeetHub

---
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var findMaxAverage = function(nums, k) {
+    let maxSum = 0;
+    for (let i = 0; i < k; i++) {
+        maxSum += nums[i];
+    }
+
+    let currentSum = maxSum;
+    for (let i = k; i < nums.length; i++) {
+        currentSum += nums[i] - nums[i - k];
+        maxSum = Math.max(maxSum, currentSum);
+    }
+
+    return maxSum / k;
+    
+};
