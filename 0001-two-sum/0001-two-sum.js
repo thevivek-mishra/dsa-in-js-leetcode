d65906859c66a9459ac219f5cc490dbc16380e3d
Vivek Kumar Mishra <155726934+thevivek-mishra@users.noreply.github.com>
2025-06-27 22:55:21 +0530

    Time: 29 ms (44.75%), Space: 55.5 MB (30.92%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -3,12 +3,36 @@
  * @param {number} target
  * @return {number[]}
  */
+
+//  brute force appraoch  
+
 var twoSum = function(nums, target) {
-    for(let i=0; i < nums.length; i++){
-        for(let j = i+1; j< nums.length; j++){
-            if(nums[i]+nums[j] === target ){
-               return [i, j]
-               }
+    for(let i=0; i< nums.length; i++){
+        for(let j=i+1; j< nums.length; j++){
+           if ( nums[i] + nums[j] === target){
+            return [i,j]
+           } 
         }
     }
 };
+
+// optimal solutions 
+// this will not work here because array is not sorted then full test will not be passed os here is better and otimal apprach is hashmap
+
+// var twoSum = function(nums, target) {
+//     let left =0;
+//     let right = nums.length -1;
+
+//     while(left <right){
+//         let sum = nums[left] + nums[right] 
+//         if(sum === target ){
+
+//             return [left, right]
+//         }else if( sum < target){
+//             left++
+//         }else {
+//             right--
+//         }
+//     }
+//     return []
+// }
